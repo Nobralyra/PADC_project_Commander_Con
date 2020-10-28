e918b7a438b525159c7c97b8f4236276c1b12354
Nobralyra <signees96@gmail.com>
2020-10-28 14:35:56 +0100

    Tilføjet tournament Controller

--- src/main/java/com/padc/demo/tournament/controller/TournamentController.java
+++ src/main/java/com/padc/demo/tournament/controller/TournamentController.java
@@ -1,5 +1,18 @@
 package com.padc.demo.tournament.controller;
 
+import com.padc.demo.core.IService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TournamentController
 {
+    private final IService iTournamentService;
+
+    // https://stackoverflow.com/questions/40620000/spring-autowire-on-properties-vs-constructor
+    @Autowired
+    public TournamentController(IService iTournamentService)
+    {
+        this.iTournamentService = iTournamentService;
+    }
 }
