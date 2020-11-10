fb5faff405b16392d1919d7b9cfb996ae701d309
Nobralyra <signees96@gmail.com>
2020-11-10 09:56:30 +0100

    Fjernet udnødvendige imports og fjernet en get()

--- src/main/java/com/padc/demo/tournament/controller/TournamentController.java
+++ src/main/java/com/padc/demo/tournament/controller/TournamentController.java
@@ -7,12 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @Controller
 public class TournamentController
@@ -30,7 +28,7 @@ public class TournamentController
     @GetMapping("/turnering/turnering_side/{id}")
     public String tournamentPage(@PathVariable("id") long id, Model model)
     {
-        Tournament tournament = iTournamentService.findById(id).get();
+        Tournament tournament = iTournamentService.findById(id);
         model.addAttribute("tournament", tournament);
 
         return ("/turnering/turnering_side");
