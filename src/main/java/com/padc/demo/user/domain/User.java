c904c82e24faf4035fc002942ac8a0416782be42
Nobralyra <signees96@gmail.com>
2020-12-01 15:25:20 +0100

    Tilføjet relation til Deck og to utility metoder

--- src/main/java/com/padc/demo/user/domain/User.java
+++ src/main/java/com/padc/demo/user/domain/User.java
@@ -2,11 +2,12 @@ package com.padc.demo.user.domain;
 
 import com.padc.demo.core.auditing.Audition;
 import com.padc.demo.core.enums.Role;
+import com.padc.demo.deck.domain.Deck;
 import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-
+import java.util.List;
 
 //https://github.com/springframeworkguru/ssc-brewery/blob/db-project-lombok/src/main/java/guru/sfg/brewery/domain/security/User.java
 @Setter
@@ -46,13 +47,49 @@ public class User extends Audition {
     private String phoneNumber;
 
 
-//    //mappedby is the tablename
-//    @OneToMany(cascade = CascadeType.ALL,
-//            fetch = FetchType.LAZY,
-//            mappedBy = "company")
-//    private List<Deck> listOfDecks;
+    /**
+     * mappedBy = "user" attribute specifies that the field "private User user;" in class Deck,
+     * owns the relationship (is the owner), and therefore contains the foreign key to query findAll
+     * decks for an user.
+     * https://en.wikibooks.org/wiki/Java_Persistence/OneToMany
+     */
+    @OneToMany(cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY,
+            mappedBy = "user")
+    private List<Deck> listOfDecks;
 
 
+    /**
+     * For adding an entity from the relationship, we need to update both sides of the association.
+     * Instead of doing it in the Service, it can be done with af utility method that updates both entities.
+     *
+     * https://thorben-janssen.com/best-practices-many-one-one-many-associations-mappings/
+     * https://en.wikibooks.org/wiki/Java_Persistence/OneToMany
+     * https://medium.com/@rajibrath20/the-best-way-to-map-a-onetomany-relationship-with-jpa-and-hibernate-dbbf6dba00d3
+     * @param deck
+     */
+    public void addDeck(Deck deck)
+    {
+        this.listOfDecks.add(deck);
+        if (deck.getUser() != this)
+        {
+            deck.setUser(this);
+        }
+    }
 
-
+    /**
+     * For removing an entity from the relationship, we need to update both sides of the association.
+     * Instead of doing it in the Service, it can be done with af utility method that updates both entities.
+     *
+     * https://thorben-janssen.com/best-practices-many-one-one-many-associations-mappings/
+     * https://en.wikibooks.org/wiki/Java_Persistence/OneToMany
+     * https://medium.com/@rajibrath20/the-best-way-to-map-a-onetomany-relationship-with-jpa-and-hibernate-dbbf6dba00d3
+     * @param deck
+     */
+    public void removeDeck(Deck deck)
+    {
+        this.listOfDecks.remove(deck);
+        deck.setUser(null);
+    }
 }
