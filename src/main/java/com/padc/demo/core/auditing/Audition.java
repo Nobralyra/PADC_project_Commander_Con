b2615cf85bf858b8153d8cfc0bc1d22be7c611e5
Päivi Eversbusch <paiv0018@stud.kea.dk>
2020-12-02 13:26:32 +0100

    Opdater bruger

--- src/main/java/com/padc/demo/core/auditing/Audition.java
+++ src/main/java/com/padc/demo/core/auditing/Audition.java
@@ -46,37 +46,30 @@ public class Audition
     {
         return createdBy;
     }
-
     public void setCreatedBy(String createdBy)
     {
         this.createdBy = createdBy;
     }
-
     public Date getCreatedDate()
     {
         return createdDate;
     }
-
     public void setCreatedDate(Date createdDate)
     {
         this.createdDate = createdDate;
     }
-
     public String getLastModifiedBy()
     {
         return lastModifiedBy;
     }
-
     public void setLastModifiedBy(String lastModifiedBy)
     {
         this.lastModifiedBy = lastModifiedBy;
     }
-
     public Date getLastModifiedDate()
     {
         return lastModifiedDate;
     }
-
     public void setLastModifiedDate(Date lastModifiedDate)
     {
         this.lastModifiedDate = lastModifiedDate;
