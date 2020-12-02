42c02279626d5e5e598ab6b2f425796a7f7a07be
Nobralyra <signees96@gmail.com>
2020-12-02 13:22:39 +0100

    Added securitycontext and query method

--- src/main/java/com/padc/demo/deck/service/DeckService.java
+++ src/main/java/com/padc/demo/deck/service/DeckService.java
@@ -1,13 +1,10 @@
 package com.padc.demo.deck.service;
 
 import com.padc.demo.core.IService;
-import com.padc.demo.core.security.UserDetailHandler;
+import com.padc.demo.core.security.Securitycontext;
 import com.padc.demo.deck.domain.Deck;
 import com.padc.demo.deck.repository.IDeckRepository;
-import com.padc.demo.user.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -19,20 +16,24 @@ import java.util.Optional;
 public class DeckService implements IService<Deck>
 {
     private final IDeckRepository iDeckRepository;
+    private final Securitycontext securitycontext;
 
     // https://stackoverflow.com/questions/40620000/spring-autowire-on-properties-vs-constructor
     @Autowired
-    public DeckService(IDeckRepository iDeckRepository)
+    public DeckService(IDeckRepository iDeckRepository, Securitycontext securitycontext)
     {
         this.iDeckRepository = iDeckRepository;
+        this.securitycontext = securitycontext;
     }
 
+    /**
+     * To save the deck it needs to be associated with the User, so User and Deck is still in sync
+     * @param element
+     */
     @Override
     public void save(Deck element)
     {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetailHandler userDetailHandler = (UserDetailHandler) authentication.getPrincipal();
-        userDetailHandler.getUser().addDeck(element);
+        securitycontext.getUserDetailHandler().getUser().addDeck(element);
         iDeckRepository.save(element);
     }
 
@@ -57,18 +58,26 @@ public class DeckService implements IService<Deck>
     @Override
     public List<Deck> findAll()
     {
-        List<Deck> deckList = new ArrayList<>();
+        List<Deck> deckListUserHas = findDecksByUser();
 
-        for (Deck deck: iDeckRepository.findAll())
-        {
-            deckList.add(deck);
-        }
-        return deckList;
+        return deckListUserHas;
     }
 
     @Override
     public void deleteByID(Long id)
     {
         iDeckRepository.deleteById(id);
+    }
+
+    public List<Deck> findDecksByUser()
+    {
+        List<Deck> deckList = new ArrayList<>();
+        Long userId = securitycontext.getUserDetailHandler().getId();
+        for (Deck deck: iDeckRepository.findDecksByUser_Id(userId))
+        {
+            deckList.add(deck);
+        }
+
+        return deckList;
     }
 }
