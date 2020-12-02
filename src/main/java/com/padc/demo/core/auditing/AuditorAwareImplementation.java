b51214fc7d155871d169635188a649cdc42219de
Nobralyra <signees96@gmail.com>
2020-12-02 10:48:34 +0100

    Deleted 3 imports

--- src/main/java/com/padc/demo/core/auditing/AuditorAwareImplementation.java
+++ src/main/java/com/padc/demo/core/auditing/AuditorAwareImplementation.java
@@ -1,8 +1,5 @@
 package com.padc.demo.core.auditing;
 
-import com.padc.demo.core.security.Securitycontext;
-import com.padc.demo.core.security.UserDetailHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
